09af0151db556fa2fa4b97e422b53fb245ee71c5
AionAdrammelech <pjosesand699@gmail.com>
2023-05-24 21:05:33 -0600

    cache app prueba

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,24 +1,70 @@
-const deliveryCache = 'delivery-cache-site-v1.0.0';
+const deliveryCache = "delivery-cache-site-v1.0.0";
 
 const datosCacheDelivery = [
-  "/index.html",
-  "/styles.css",
-  '/scrpt/scrpts.js',
-  '/img-src/pikapika.webp',
-  '/img-src/sayaka.webp',
-  '/webComponents/CardElement/cardElement.js'
+  "/",
+  "index.html",
+  "style.css",
+  "scrpts/scrpts.js",
+  "img-src/pikapika.webp",
+  "img-src/sayaka.webp",
+  "webComponents/CardElement/cardElement.js"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(deliveryCache)
-      .then(cache => {cache.addAll(datosCacheDelivery);})
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(deliveryCache);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll([
+        "/",
+        "index.html",
+        "style.css",
+        "img-src/pikapika.webp",
+        "webComponents/CardElement/cardElement.js",
+        "img-src/sayamiki.webp"
+      ]);
+    })()
   );
 });
 
-self.addEventListener('fetch', event => {
+/* self.addEventListener('install',  
+event => {
+  event.waitUntil( 
+    caches.open(deliveryCache)
+      .then(cache => {return cache.addAll([
+        "index.html",
+        "style.css",
+        "img-src/pikapika.webp",
+        "webComponents/CardElement/cardElement.js",
+        "img-src/sayamiki.webp"
+      ])})
+
+
+  );
+}); */
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(deliveryCache);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
+
+
+/* self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {return response || fetch(event.request);})
   );
-});
+}); */
